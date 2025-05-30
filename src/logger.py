5b27928d19105c7b01ae9fddc1dea2c88f4c6ad5
rithwik <rithwikvasa@gmail.com>
2025-05-30 19:43:30 +0530

    exception.py and logger.py file are created

---
+++ src/logger.py
@@ -0,0 +1,14 @@
+import logging
+import os
+from datetime import datetime
+logs_file=f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+logs_path=os.path.join(os.getcwd(),"logs")
+os.makedirs(logs_path,exist_ok=True)
+log_file_path=os.path.join(logs_path,logs_file)
+
+
+logging.basicConfig(
+    filename=log_file_path,
+    format='[%(asctime)s] %(lineno)d %(name)s-%(levelname)s-%(message)s',
+    level=logging.INFO
+)
